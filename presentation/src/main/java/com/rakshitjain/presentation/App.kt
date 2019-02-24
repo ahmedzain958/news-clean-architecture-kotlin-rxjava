35eedc42304ace8ecc436c40446f967a94e2a323
ahmedzain958 <ahmedzain958@gmail.com>
2019-02-24 20:34:30 +0200

    Initial commit

---
+++ presentation/src/main/java/com/rakshitjain/presentation/App.kt
@@ -0,0 +1,31 @@
+package com.rakshitjain.presentation
+
+import android.app.Application
+import com.rakshitjain.presentation.di.*
+import org.koin.android.ext.android.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        loadKoin()
+    }
+
+    private fun loadKoin() {
+        startKoin(this,
+                listOf(mNetworkModules,
+                        mViewModels,
+                        mRepositoryModules,
+                        mUseCaseModules,
+                        mLocalModules)
+
+        )
+    }
+
+
+}
+
+/*
+fun main(args: Array<String>) {
+    println("saaaaaaa")
+}*/
